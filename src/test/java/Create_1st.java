a1417dcd0018f7ae5958b0457550ece648094d61
DELL <DELL@Vivek>
2022-11-22 10:39:28 +0530

    framework

---
+++ src/test/java/Create_1st.java
@@ -0,0 +1,19 @@
+
+
+public class Create_1st {
+	public static void main(String[]args) {
+		
+		
+		System.out.println("created 1st time");
+
+		System.out.println("updated");
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
